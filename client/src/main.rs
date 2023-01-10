e742a4400b13878d754a9eb41357d65cb5d5ab3f
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-10 20:23:58 +0000

    Implement some basic sending of data for a client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,23 @@
+use std::net::TcpStream;
+use std::{
+    io,
+    io::{Read, Write},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = [0; 1024];
+
+    let mut input = String::new();
+
+    match TcpStream::connect("pc.local:7878") {
+        Ok(mut stream) => {
+            stream.read(&mut buf).unwrap();
+
+            stream.write(b"Response").unwrap();
+            stream.flush().unwrap();
+        }
+        Err(err) => {
+            println!("Error: {err}");
+        }
+    }
 }
