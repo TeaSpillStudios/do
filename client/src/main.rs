ab7b90ded718c5337e78e04d4d37a44c521b86a1
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-10 21:00:11 +0000

    Send a RON request to the server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,19 +1,24 @@
 use requests::*;
+use ron::{de::from_str, ser::to_string};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() {
+    let request = Requests::Send(Request {
+        operation: Operation::Add,
+        section: Some(String::from("A test section")),
+        name: Some(String::from("Test")),
+        data: Some(String::from("A test task")),
+    });
+
+    let data = to_string(&request).unwrap();
+
     let mut buf = [0; 1024];
 
-    match TcpStream::connect("pc.local:7878") {
-        Ok(mut stream) => {
-            stream.read(&mut buf).unwrap();
+    let mut stream = TcpStream::connect("pc.local:7878").unwrap();
 
-            stream.write(b"Response").unwrap();
-            stream.flush().unwrap();
-        }
-        Err(err) => {
-            println!("Error: {err}");
-        }
-    }
+    // stream.read(&mut buf).unwrap();
+
+    stream.write_all(data.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
