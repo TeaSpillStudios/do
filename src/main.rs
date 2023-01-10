642b40442a077d1559d2d25dc999f8371f3cba97
TeaSpillStudios <teaspillstudios@proton.me>
2023-01-10 19:30:50 +0000

    Implement basic TcpListener and connection handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+// use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("pc.local:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    stream.read(&mut buf).unwrap();
+
+    let output = b"Response test";
+
+    stream.write_all(output).unwrap();
+    stream.flush().unwrap();
 }
